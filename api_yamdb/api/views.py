fde3fe8447efaa1fc71f2d684bfa4e3737fb6793
ilyaryk <77522160+ilyaryk@users.noreply.github.com>
2023-04-22 16:50:38 +0700

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,36 +1,24 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.core.mail import send_mail
+from django.db.models import Avg
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import (filters, mixins, permissions, status, views,
+                            viewsets)
+from rest_framework.decorators import action
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from reviews.models import Category, Genre, Review, Title
+from users.models import User
 
-from api.views import (AuthSignupView, CommentViewSet, GetJWTTokenView,
-                       ReviewViewSet, UsersViewSet)
-
-from .views import CategoryViewSet, GenreViewSet, TitleViewSet
-
-app_name = "api"
-
-router_v1 = DefaultRouter()
-router_v1.register(r"users", UsersViewSet, basename="users")
-
-router_v1.register(r"categories", CategoryViewSet, basename="category")
-router_v1.register(r"genres", GenreViewSet, basename="genre")
-router_v1.register(r"titles", TitleViewSet, basename="title")
-router_v1.register(
-    r"titles/(?P<title_id>\d+)/reviews", ReviewViewSet, basename="review"
-)
-
-router_v1.register(
-    r"titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments",
-    CommentViewSet,
-    basename="comment",
-)
-
-urlpatterns = [
-    path("v1/auth/signup/", AuthSignupView.as_view()),
-    path("v1/auth/token/", GetJWTTokenView.as_view()),
-    path("v1/", include(router_v1.urls)),
-]
-
-from reviews.models import Category, Title, Genre, Review
+from .filters import TitleFilter
+from .permissions import AuthorModeratorOrReadOnly, IsAdmin
+from .serializers import (AuthSignupSerializer, CategorySerializer,
+                          CommentSerializer, GenreSerializer,
+                          GetJWTTokenSerializer, ReviewSerializer,
+                          TitleListSerializer, TitleSerializer,
+                          UserViewSerializer)
 
 EMAIL_TITLE = "Приветствуем {}"
 EMAIL_MESSAGE = "Ваш секретный код: {}"
